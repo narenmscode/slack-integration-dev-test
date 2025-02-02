db58ba5c013cb6ed138e28b067f83063b04861c5
narenmscode <narendranworkmail@gmail.com>
2025-02-02 17:18:39 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+
+  const handleSendMessage = () =>{
+    
+  }
+
+  return (
+    <>
+      <div>
+        <button onClick={handleSendMessage}>send message</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
